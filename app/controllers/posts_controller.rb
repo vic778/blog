c98f2ab336bc20b22ec5f2ce8efabb8ed39092c8
vic778 <victoiremmanuelbarh@gmail.com>
2022-03-22 10:04:15 +0300

    Create action to edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,53 @@
 class PostsController < ApplicationController
 
     def index
+      @post = Post.all.order("created_at DESC")
     end
 
     def new
-        
+      @post = Post.new
     end
 
     def create
-        
+      @post = Post.new(post_params)
+
+      if @post.save 
+        redirect_to @post
+      else
+        render 'new'
+      end
+    end
+
+    def show
+      @post = Post.find(params[:id])
+    end
+
+    def update
+      @post = Post.find(params[:id])
+
+      if @post.update(post_params)
+        redirect_to @post
+      else
+        render 'edit'
+      end
+
+    end
+
+    def edit
+      @post = Post.find(params[:id])
+    end
+
+    def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+
+      redirect_to posts_path
+    end
+
+
+    private 
+
+    def post_params
+      params.require(:post).permit(:title, :content)
     end
 end
