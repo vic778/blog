d2c05f8373e74489cf5a7aec916a090f728fe1de
vic778 <victoiremmanuelbarh@gmail.com>
2022-03-22 19:07:03 +0300

    aa

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :update, :edit, :destroy]
 
     def index
       @post = Post.all.order("created_at DESC")
@@ -19,11 +20,11 @@ class PostsController < ApplicationController
     end
 
     def show
-      @post = Post.find(params[:id])
+      # @post = Post.find(params[:id])
     end
 
     def update
-      @post = Post.find(params[:id])
+      # @post = Post.find(params[:id])
 
       if @post.update(post_params)
         redirect_to @post
@@ -34,14 +35,18 @@ class PostsController < ApplicationController
     end
 
     def edit
-      @post = Post.find(params[:id])
+      # @post = Post.find(params[:id])
     end
 
     def destroy
-      @post = Post.find(params[:id])
+      # @post = Post.find(params[:id])
       @post.destroy
 
       redirect_to posts_path
+    end
+
+    def find_post
+      @post = Post.find(params[:id])
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   root "posts#index"
+  
 end
