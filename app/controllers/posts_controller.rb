25cda92303d0a39c99ad32fee26d5e9883f115bb
vic778 <victoiremmanuelbarh@gmail.com>
2022-03-28 09:30:11 +0300

    Upload image

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -53,6 +53,6 @@ class PostsController < ApplicationController
     private 
 
     def post_params
-      params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :content, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_22_084533) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_28_052858) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_22_084533) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
