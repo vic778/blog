f6fce120783504ba67ccb4f7d270935d300be672
vic778 <victoiremmanuelbarh@gmail.com>
2022-04-16 14:11:39 +0300

    Fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,58 +1,56 @@
 class PostsController < ApplicationController
-  before_action :find_post, only: [:show, :update, :edit, :destroy]
+  before_action :find_post, only: %i[show update edit destroy]
 
-    def index
-      @post = Post.all.order("created_at DESC")
+  def index
+    @post = Post.all.order('created_at DESC')
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
     end
+  end
 
-    def new
-      @post = Post.new
+  def show
+    # @post = Post.find(params[:id])
+  end
+
+  def update
+    # @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
     end
+  end
 
-    def create
-      @post = Post.new(post_params)
+  def edit
+    # @post = Post.find(params[:id])
+  end
 
-      if @post.save 
-        redirect_to @post
-      else
-        render 'new'
-      end
-    end
+  def destroy
+    # @post = Post.find(params[:id])
+    @post.destroy
 
-    def show
-      # @post = Post.find(params[:id])
-    end
+    redirect_to posts_path
+  end
 
-    def update
-      # @post = Post.find(params[:id])
+  def find_post
+    @post = Post.find(params[:id])
+  end
 
-      if @post.update(post_params)
-        redirect_to @post
-      else
-        render 'edit'
-      end
+  private
 
-    end
-
-    def edit
-      # @post = Post.find(params[:id])
-    end
-
-    def destroy
-      # @post = Post.find(params[:id])
-      @post.destroy
-
-      redirect_to posts_path
-    end
-
-    def find_post
-      @post = Post.find(params[:id])
-    end
-
-
-    private 
-
-    def post_params
-      params.require(:post).permit(:title, :content, :image)
-    end
+  def post_params
+    params.require(:post).permit(:title, :content, :image)
+  end
 end
